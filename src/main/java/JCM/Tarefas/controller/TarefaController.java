c7fa70fdc4bad47567eea4efdb6bc9357872e77e
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2021-05-26 14:54:26 -0300

    adicionado camada service

--- src/main/java/JCM/Tarefas/controller/TarefaController.java
+++ src/main/java/JCM/Tarefas/controller/TarefaController.java
@@ -15,47 +15,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import JCM.Tarefas.model.Tarefa;
-import JCM.Tarefas.repository.TarefaRepository;
+import JCM.Tarefas.services.TarefaService;
 
 @RestController
 public class TarefaController {
 	
 	@Autowired
-	private TarefaRepository repositorio;
+	private TarefaService service;
 	
-	@GetMapping("/tarefa")
+	@GetMapping("/tarefalike")
 	public List<Tarefa> todasTarefas(@RequestParam Map<String, String> parametros){
 		if(parametros.isEmpty())
-			return repositorio.findAll();
+			return service.getTodasTarefas();
 		
 		String descricao = parametros.get("descricao");
-		return repositorio.findByDescricao(descricao);
+		return service.getTarefasPorDescricao(descricao);
 		
 	}
 	
-	@GetMapping("/tarefalike")
-	public List<Tarefa> todasTarefas2(@RequestParam Map<String, String> parametros){
-		if(parametros.isEmpty())
-			return repositorio.findAll();
-		
-		String descricao = parametros.get("descricao");
-		return repositorio.findByDescricaoLike("%" + descricao + "%");
-		
+	@GetMapping("/tarefa")
+	public List<Tarefa> todasTarefas2(){
+
+			return service.getTodasTarefas();
 	}
 	
 	@GetMapping("/tarefa/{id}")
 	public Tarefa umaTarefa(@PathVariable Integer id) {
-		return repositorio.findById(id).orElse(null);
+		return service.getTarefaPorId(id);
 	}
 	
 	@PostMapping("/tarefa")
 	public Tarefa salvarTarefa(@Valid @RequestBody Tarefa tarefa) {
-		return repositorio.save(tarefa);
+		return service.salvarTarefa(tarefa);
 	}
 	
 	@DeleteMapping("/tarefa/{id}")
 	public void excluirTarefa(@PathVariable Integer id) {
-		repositorio.deleteById(id);
+		service.deleteById(id);
 	}
 	
 
